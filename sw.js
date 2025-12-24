de41550c802b9af2cdb05e318622f4e966a33b95
albert11a <64289532+albert11a@users.noreply.github.com>
2025-12-24 18:11:00 +0100

    uh

---
+++ sw.js
@@ -0,0 +1,8 @@
+// MENYRA noop service worker: no caching, no fetch handling.
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
