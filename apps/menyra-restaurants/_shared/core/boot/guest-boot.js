f4d715a3209afb096ddfc6e93e155fe25b09d131
albert11a <64289532+albert11a@users.noreply.github.com>
2025-12-25 09:17:09 +0100

    i

--- apps/menyra-restaurants/_shared/core/boot/guest-boot.js
+++
@@ -1,1 +0,0 @@
-import {getContext,requireRestaurant} from '../context.js'; export const guestBoot=()=>{const ctx=getContext();return {ctx,ok:requireRestaurant(ctx)}};
--- apps/menyra-restaurants/guest/porosia/porosia.js
+++
@@ -1,5 +0,0 @@
-// apps/menyra-restaurants/guest/porosia/porosia.js
-import { bootCommon, initPorosia } from "../_shared/guest-core.js";
-
-bootCommon();
-initPorosia();
